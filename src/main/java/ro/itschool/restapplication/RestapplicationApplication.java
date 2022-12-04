38fb254609db7324ee50d17b80decf9c50879dfe
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-12-04 19:29:07 +0200

    First commit

---
+++ src/main/java/ro/itschool/restapplication/RestapplicationApplication.java
@@ -0,0 +1,13 @@
+package ro.itschool.restapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/itschool/restapplication/RestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.itschool.restapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
