fb418d1c2b5ff037f9891b91065508dc03d2163c
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-12-04 20:29:47 +0200

    find all books method

--- src/main/java/ro/itschool/restapplication/controller/MyRestController.java
+++ src/main/java/ro/itschool/restapplication/controller/MyRestController.java
@@ -1,13 +1,23 @@
 package ro.itschool.restapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ro.itschool.restapplication.entity.Book;
+import ro.itschool.restapplication.repository.BookRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value="/book")
 public class MyRestController {
 
-    @GetMapping(value="/get-example")
-    public String sayHello() {
-        return "Hello World!";
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping(value="/all")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
