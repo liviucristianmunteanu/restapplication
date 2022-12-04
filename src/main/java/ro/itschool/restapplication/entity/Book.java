2e183149b36924ea5df6564dc766de0ec184ee7e
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-12-04 20:35:39 +0200

    added book name

--- src/main/java/ro/itschool/restapplication/entity/Book.java
+++ src/main/java/ro/itschool/restapplication/entity/Book.java
@@ -6,16 +6,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private String author;
     private String genre;
 }
